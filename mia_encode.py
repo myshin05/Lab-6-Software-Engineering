4c634d5ee491288f09149aed0d1c446c180a76ba
lucaweisman <148773945+lucaweisman@users.noreply.github.com>
2023-10-25 17:36:43 -0400

    added luca_decode.py

--- mia_encode.py
+++ mia_encode.py
@@ -1,3 +1,17 @@
+# luca weisman
+
+def decode(encoded_password):
+  original_password = ""
+  for digit in encoded_password:
+    temp = int(digit) - 3
+    if temp < 0:
+      temp += 10
+      original_password += str(temp)
+    else:
+      original_password += str(temp)
+
+  return original_password
+
 # mia shin
 
 def encode(number):     # encode function, moves up each digit of the 8-digit number by 3
